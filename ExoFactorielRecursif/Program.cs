fa1ae69945091bc7cc7aa9bbf3cff4d06ec242a3
TMCognitic <thierry.morre@cognitic.be>
2021-12-08 13:52:09 +0100

    Ajoutez des fichiers projet.

---
+++ ExoFactorielRecursif/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ExoFactorielRecursif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte value; //Factoriel maximum calculable est 27 => valeur maximale du sbyte est 127
+
+            do
+            {
+                Console.Clear();
+                Console.Write("Veuillez entrer un nombre > 1 : ");
+            } while (!sbyte.TryParse(Console.ReadLine(), out value) || value < 1);
+
+            Console.WriteLine(Factoriel(value));
+        }
+
+        static decimal Factoriel(decimal x)
+        {
+            if(x < 1) 
+                return -1;
+
+            return (x == 1) ? 1 : x * Factoriel(x - 1);
+        }
+    }
+}
+
+/*
+ * 5! => 5 * 4 * 3 * 2 * 1
+ * 4! => 4 * 3 * 2 * 1
+ * 
+ * 5! => 5 * 4!
+ * 4! => 4 * 3!
+ * 1! = 1
+ */
